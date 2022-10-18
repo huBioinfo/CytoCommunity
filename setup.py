f1f62cae4cc856d7c86d7131abcf7b80b4209511
yafeixu-xidian <115676672+yafeixu-xidian@users.noreply.github.com>
2022-10-18 10:21:05 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cyto community',
+    version='1.0.0',
+    author='abc',
+    author_email='',
+    url='',
+    license='',
+    maintainer='',
+    description='',
+    packages=find_packages(),
+)

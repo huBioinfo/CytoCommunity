ad46eb56c57e1b4fa9f56b91382e619e3ddeda9c
yafeixu-xidian <115676672+yafeixu-xidian@users.noreply.github.com>
2022-11-07 16:53:06 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     url='https://github.com/huBioinfo/CytoCommunity',
     license='',
     maintainer='yuxuanHu',
-    description='CytoCommunity: a deep graph learning approach for identification of tissue cellular neighborhoods based on cell phenotypes and cell spatial distributions',
+    description='A deep graph learning approach for identification of tissue cellular neighborhoods based on cell phenotypes and cell spatial distributions',
     packages=find_packages(),
 )
